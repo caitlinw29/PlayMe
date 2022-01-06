d304c53d1cb4dbb49198ba4a6327b7a4ab4f2527
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-06 13:08:17 -0600

    This commit includes the creation of a favorites page that also includes a clickable list of activities for returning to previous activities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 
-// this is what a response from the bored API looks like.  variables for storage will bebased on this
+// All of this code is commented out by Mike so that he can utilize it on his own computer without messing up anything that other people are working on. 
+
+// This is what a response from the bored API looks like.  variables for storage will be based on this FormData.
 // {
 // 	"activity": "Learn a new programming language",
 // 	"accessibility": 0.25,
@@ -8,15 +10,44 @@
 // 	"price": 0.1,
 // 	"key": "5881028"
 // }
+// {
+// "activity": "Basic Knife Skills",
+// 	"accessibility": 0.25,
+// 	"type": "education",
+// 	"participants": 1,
+// 	"price": 0.1,
+// 	"key": "1234567"
+// }
 
+// // {"activity": "Learn How to Build a Snowman",
+// // "accessibility": 0.25,
+// // "type": "education",
+// // "participants": 1,
+// // "price": 0.1,
+// // "key": "7654321"
+// // }
 
+// // this is code that was 
 // function storeData() {
+
+// save to favorites button returns save to favorites
+document.getElementById("fvBtn").addEventListener
+    ("click", function(){
+        console.log("Save to Favorites")
+    });
+
+
+
+    // {
+    //       document.getElementById("card-title").innerHTML = "Hello World";
+    //     console.log("favBtn");
+    //     });
   
-//     document.querySelectorAll(".saveBtn").forEach((svBtn) => {
-//        svBtn.addEventListener('click', (event) => {
+//  
+//    element.addEventListener('mousedown', (event) => {
 //          event.preventDefault();
    
-//    var entryId = svB.id
+//    var entryId = element.id
    
 //    var entry = element.previousElementSibling.value;
 //    console.log('entry', entry);
@@ -24,15 +55,31 @@
 //    var addEntry  = { 
 //      "id": entryId, 
 //      "entry": entry,
-//      }
+//      } 
    
-//      var apiActivity = JSON.parse(localStorage.getItem("API card")) || [];
+//      var existingSchedule = JSON.parse(localStorage.getItem("allSchedules")) || [];
    
 //      localStorage.setItem("addEntry", JSON.stringify(addEntry)) 
-//      apiActivity.push(addEntry)
-//      localStorage.setItem("API card", JSON.stringify(apiActivity))
+//      existingSchedule.push(addEntry)
+//      localStorage.setItem("allSchedules", JSON.stringify(existingSchedule))
    
 //        })
 //      })
 //    }
-//    storeData
+//    storeData()
+   
+//    function retrieveData() { 
+//    var existingSchedules = JSON.parse(localStorage.getItem("allSchedules"));
+//    existingSchedules.forEach((schedule)=> {
+//      var eventItem = schedule.entry;
+//      var saveBtn = schedule.id;
+//      var saveId = document.getElementById(`${saveBtn}`)
+//      var eventSpot = saveId.previousElementSibling;
+//      eventSpot.value = eventItem
+   
+     
+//    })  
+   
+//    }
+   
+//    retrieveData();

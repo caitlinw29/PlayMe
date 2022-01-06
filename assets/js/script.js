4b06a78f9771b72903494a644b40614eca8a7b8e
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-06 10:04:50 -0600

    This update includes new html text fro a Favortite Activites page.  It also includes a plain materialize card.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
-//JS
+
+// this is what a response from the bored API looks like.  variables for storage will bebased on this
+// {
+// 	"activity": "Learn a new programming language",
+// 	"accessibility": 0.25,
+// 	"type": "education",
+// 	"participants": 1,
+// 	"price": 0.1,
+// 	"key": "5881028"
+// }
+
+
+// function storeData() {
+  
+//     document.querySelectorAll(".saveBtn").forEach((svBtn) => {
+//        svBtn.addEventListener('click', (event) => {
+//          event.preventDefault();
+   
+//    var entryId = svB.id
+   
+//    var entry = element.previousElementSibling.value;
+//    console.log('entry', entry);
+   
+//    var addEntry  = { 
+//      "id": entryId, 
+//      "entry": entry,
+//      }
+   
+//      var apiActivity = JSON.parse(localStorage.getItem("API card")) || [];
+   
+//      localStorage.setItem("addEntry", JSON.stringify(addEntry)) 
+//      apiActivity.push(addEntry)
+//      localStorage.setItem("API card", JSON.stringify(apiActivity))
+   
+//        })
+//      })
+//    }
+//    storeData

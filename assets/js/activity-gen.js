0b9927c606680ddb7c558bf513327b540daa73c2
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-07 18:49:22 -0500

    Add card to page and dynamically change text with JS fetch request

--- assets/js/activity-gen.js
+++ assets/js/activity-gen.js
@@ -1,4 +1,10 @@
 const randomBtn = document.getElementById("findActivityBtn");
+const activity = document.getElementById("activity");
+const type = document.getElementById("type");
+const participants = document.getElementById("participants");
+const price = document.getElementById("price");
+const link = document.getElementById("link");
+const activityCardImg = document.getElementById("activityCardImg");
 
 randomBtn.addEventListener("click", generateRandomActivityCard);
 
@@ -10,5 +16,24 @@ function generateRandomActivityCard(){
       })
       .then(function (data) { 
         console.log(data);
+        activity.textContent = data.activity;
+        type.textContent = data.type;
+        participants.textContent = data.participants;
+        let symbolPrice;
+        if(data.price > 0 && data.price < .1){
+          symbolPrice = "FREE";
+        } else if (data.price >= .1 && data.price < .3){
+          symbolPrice = "$";
+        } else if (data.price >= .3 && data.price < .6){
+          symbolPrice = "$$";
+        } else if (data.price >= .6 && data.price < .9){
+          symbolPrice = "$$$";
+        } else if (data.price >= .9){
+          symbolPrice = "$$$$";
+        }
+        price.textContent = symbolPrice;
+        if (data.link !== ""){
+          link.textContent = data.link;
+        }
       })
 }

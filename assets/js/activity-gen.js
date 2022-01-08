5b6e234be1e052515c217d4a0d091c11cd338bda
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-07 20:07:50 -0500

    Set up image request

--- assets/js/activity-gen.js
+++ assets/js/activity-gen.js
@@ -5,10 +5,12 @@ const participants = document.getElementById("participants");
 const price = document.getElementById("price");
 const link = document.getElementById("link");
 const activityCardImg = document.getElementById("activityCardImg");
+let activityString = "";
 
 randomBtn.addEventListener("click", generateRandomActivityCard);
 
 function generateRandomActivityCard(){
+  
   var randomActivityURL = "http://www.boredapi.com/api/activity/"
   fetch(randomActivityURL)
       .then(function (response) {
@@ -16,7 +18,8 @@ function generateRandomActivityCard(){
       })
       .then(function (data) { 
         console.log(data);
-        activity.textContent = data.activity;
+        activityString = data.activity;
+        activity.textContent = activityString;
         type.textContent = data.type;
         participants.textContent = data.participants;
         let symbolPrice;
@@ -36,4 +39,21 @@ function generateRandomActivityCard(){
           link.textContent = data.link;
         }
       })
+  fetch("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=1&autoCorrect=true", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+      "x-rapidapi-key": "028b2f00a2msh04217c3fa191984p185e73jsn48767f836887"
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      activityCardImg.src = data.value[0].thumbnail;
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }

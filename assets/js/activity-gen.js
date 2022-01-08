d43e3e6b1f7349d7c73540724c335d7552f7254f
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-07 21:20:18 -0500

    Fix image request, add comments

--- assets/js/activity-gen.js
+++ assets/js/activity-gen.js
@@ -17,13 +17,18 @@ function generateRandomActivityCard(){
         return response.json();
       })
       .then(function (data) { 
-        console.log(data);
-        activityString = data.activity;
-        activity.textContent = activityString;
+        //set the activityString up to plug into the imageURL
+        activityString = data.activity.toLowerCase();
+        var activityArray = activityString.split(" ");
+        activityString = activityArray.join("%20");
+
+        //change the textContent of the card
+        activity.textContent = data.activity;
         type.textContent = data.type;
         participants.textContent = data.participants;
         let symbolPrice;
-        if(data.price > 0 && data.price < .1){
+        //set up price ranges
+        if(data.price >= 0 && data.price < .1){
           symbolPrice = "FREE";
         } else if (data.price >= .1 && data.price < .3){
           symbolPrice = "$";
@@ -35,25 +40,30 @@ function generateRandomActivityCard(){
           symbolPrice = "$$$$";
         }
         price.textContent = symbolPrice;
+        //if a link exists, plug it in to the
         if (data.link !== ""){
-          link.textContent = data.link;
+          link.href = data.link;
+          link.textContent = data.activity;
         }
+        //!! DO NOT ADD THIS IN YET. IT ADDS THE PICTURES BUT CAITLIN HAS TO PAY PAST A CERTAIN NUMBER OF API CALLS, SO WE ARE LEAVING IT OUT UNTIL PRESENTATION
+        //!! THERE IS A HARDCODED IMAGE IN THE HTML, USE THAT FOR PRACTICE/MESSING WITH THE BUTTON 
+        // var imageURL = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=" + activityString + "&pageNumber=1&pageSize=1&autoCorrect=true";
+        // fetch(imageURL, {
+        //   "method": "GET",
+        //   "headers": {
+        //     "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+        //     "x-rapidapi-key": "028b2f00a2msh04217c3fa191984p185e73jsn48767f836887"
+        //   }
+        // })
+        //   .then(response => {
+        //     return response.json();
+        //   })
+        //   .then(data => {
+        //     console.log(data);
+        //     activityCardImg.src = data.value[0].thumbnail;
+        //   })
+        //   .catch(err => {
+        //     console.error(err);
+        //   });
       })
-  fetch("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=1&autoCorrect=true", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-      "x-rapidapi-key": "028b2f00a2msh04217c3fa191984p185e73jsn48767f836887"
-    }
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      activityCardImg.src = data.value[0].thumbnail;
-    })
-    .catch(err => {
-      console.error(err);
-    });
 }

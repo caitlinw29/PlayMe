215f51374473c048f8735fbb2148cc0e546df492
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-08 13:15:08 -0500

    Add favorites list dynamically with JS, add styling to list

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -8,12 +8,30 @@ var existingActivities =
 
 if(existingActivities.length > 0 ) {
     for (var i = 0; i < existingActivities.length; i++){
+        var currentActivity = existingActivities[i];   
         var activityNewElement = document.createElement("div");
-        var activities = existingActivities[i];   
-        activityNewElement.setAttribute("id", "activity-" + i);
-        activityNewElement.textContent = activities.name;
+        activityNewElement.className = "teal row";
         activityNewElement.setAttribute("style", "margin-bottom:1%; padding:1%");
-        activityNewElement.classList.add("teal")
+        activityNewElement.setAttribute("id", "activity-" + i);
+
+        var activityCol = document.createElement("div");
+        var typeCol = document.createElement("div");
+        var participantsCol = document.createElement("div");
+        var priceCol = document.createElement("div");
+        var deleteCol = document.createElement("div");
+        activityCol.className = "col s4";
+        typeCol.className = "col s2";
+        participantsCol.className = "col s2";
+        priceCol.className = "col s2";
+        deleteCol.className = "col s2";
+
+        
+        activityCol.textContent = currentActivity.name;
+        typeCol.textContent = currentActivity.type;
+        participantsCol.textContent = currentActivity.participants;
+        priceCol.textContent = currentActivity.price;
+        deleteCol.textContent = "X";
+        activityNewElement.append(activityCol, typeCol, participantsCol, priceCol, deleteCol);
         collectionElement.append(activityNewElement);
     }
   

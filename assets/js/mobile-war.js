a0cdda6fd2962b9a25c2270b5c22d14f679e6b79
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-08 18:09:26 -0500

    Add comments and pull out unneeded scripts and files

--- assets/js/mobile-war.js
+++ assets/js/mobile-war.js
@@ -1,7 +1,8 @@
+//specifically looks to see if the page is smaller than 500px on page load
+//if it is, goes to mobile-war.html instead (completely different layout)
 function getTemplate() {
     if (screen.width < 500) {
       return window.location.replace("mobile-war.html");
     }
 }
 getTemplate();
-
